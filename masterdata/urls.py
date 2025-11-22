a113f17f6805a06e48d379110979635542cf5c4a
Ralf Hundertmark <ralf@ait.co.th>
2025-11-22 11:21:43 +0700

    Refactor URL and view definitions: standardize string quotes and improve code readability

--- masterdata/urls.py
+++ masterdata/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name = 'masterdata'
+app_name = "masterdata"
 
 urlpatterns = [
-    path('airlines/', views.airline_list, name='airline_list'),
-    path('aircraft/', views.aircraft_list, name='aircraft_list'),
-    path('terminals/', views.terminal_list, name='terminal_list'),
-    path('gates/', views.gate_list, name='gate_list'),
-    path('stands/', views.stand_list, name='stand_list'),
-    path('checkin/', views.checkin_list, name='checkin_list'),
-    path('carousels/', views.carousel_list, name='carousel_list'),
+    path("airlines/", views.airline_list, name="airline_list"),
+    path("aircraft/", views.aircraft_list, name="aircraft_list"),
+    path("terminals/", views.terminal_list, name="terminal_list"),
+    path("gates/", views.gate_list, name="gate_list"),
+    path("stands/", views.stand_list, name="stand_list"),
+    path("checkin/", views.checkin_list, name="checkin_list"),
+    path("carousels/", views.carousel_list, name="carousel_list"),
 ]
