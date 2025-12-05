9ef1cb541b7b486b57c074e439cd45d61509e827
Ralf Hundertmark <ralf@ait.co.th>
2025-12-05 20:07:00 +0700

    smaller fixes

--- os_ams/settings.py
+++ os_ams/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = "django-insecure-7&y_aar7dcifl&v7*unpv^q0zz530pc-zsm8z*u(nr8c+6vyma
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "192.168.1.34", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "osams", ""]
 
 
 # Application definition
